1082caea007b42658fca82bbb8dd0dc31e48a46f
Bi12-173 <hnguyenvuviet@gmail.com>
2023-11-17 11:20:44 +0700

    manage customer information

---
+++ lang/vi/customer.php
@@ -0,0 +1,15 @@
+<?php
+return[
+
+    'Customer' => 'Khách hàng',
+    'Customer name' => 'Tên khách hàng',
+    'Phone number' => 'Số điện thoại',
+    'Customer email' => 'Email khách hàng',
+    'Customer Face' => 'Nhân dạng',
+    'Customer address' => 'Địa chỉ',
+    'Customer Zalo' => 'Zalo khách hàng',
+    'Customer credit' => 'Điểm tín dụng',
+    'Customer age' => 'Tuổi khách hàng',
+    'Customer image' => 'Ảnh khách hàng'
+
+];
