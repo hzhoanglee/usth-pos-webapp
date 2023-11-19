1ffdac43cd43bb17151ae4c74315f1ca4796ffde
Bi12-173 <hnguyenvuviet@gmail.com>
2023-11-19 14:20:09 +0700

    manage product

---
+++ lang/vi/product.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'Product' => 'Sản phẩm',
+  'Product name' => 'Tên sản phẩm',
+  'Product description' => 'Mô tả sản phẩm',
+    'Product image' => 'Ảnh sản phẩm',
+    'Quantity' => 'Số lượng',
+    'Tax' => 'Thuế',
+    'Price box listing' => 'Giá niêm yết theo hộp',
+    'Price box discounted' => 'Giá khuyến mại theo hộp',
+    'Price item listing' => 'Giá niêm yết sản phẩm',
+    'Price item discounted' => 'Giá khuyến mại sản phẩm',
+    'Limit by age' => 'Giới hạn độ tuổi',
+    'Limit per order' => 'Giới hạn đơn hàng',
+    'SKU' => 'Mã đơn hàng',
+    'Barcode' => 'Mã vạch'
+
+
+];
