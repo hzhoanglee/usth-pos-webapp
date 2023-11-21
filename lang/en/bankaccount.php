48163ed9363d60cd0710e052037a4ba21e57f905
Bi12-173 <hnguyenvuviet@gmail.com>
2023-11-21 16:16:57 +0700

    bank account detail management

---
+++ lang/en/bankaccount.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+  'Bank number' => "Bank number",
+    'Owner name' => 'Owner name',
+    'Bank' => 'Bank'
+];
---
+++ lang/vi/bankaccount.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'Bank number' => 'Số tài khoản',
+    'Owner name' => 'Chủ tài khoản',
+    'Bank' => 'Ngân hàng'
+];
